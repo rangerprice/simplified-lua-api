0dd42f04b635654ca979c423ecd058f2d7bea89a
= <=>
2015-08-30 19:16:05 -0300

    update 0.1

---
+++ examples/script.lua
@@ -0,0 +1,11 @@
+var = "Banana :hap: "
+var2 = {"lemon", "apple", "tangerine", "orange", "pineapple"}
+var3 = 55
+var4 = 55.55
+
+function printThis(data) 
+	print(data)
+
+	return "yes", "Hello ! ", "success !"
+end
+
